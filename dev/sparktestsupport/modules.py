4725d32afd80c0954c63ec85d3f2c31d59c77d46
Robert Kruszewski <robertk@palantir.com>
2017-03-21 09:38:26 +0000

    exclude kubernetes

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -491,14 +491,6 @@ mesos = Module(
     sbt_test_goals=["mesos/test"]
 )
 
-kubernetes = Module(
-    name="kubernetes",
-    dependencies=[],
-    source_file_regexes=["resource-managers/kubernetes/core/"],
-    build_profile_flags=["-Pkubernetes"],
-    sbt_test_goals=[]
-)
-
 # The root module is a dummy module which is used to run all of the tests.
 # No other modules should directly depend on this module.
 root = Module(
