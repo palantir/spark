30f162b455902f1048f1506ac3f15d83b14cabbe
Andrew Ash <andrew@andrewash.com>
2017-05-15 17:06:30 -0700

    Publish docker images

--- dev/publish.sh
+++ dev/publish.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 version=$(git describe --tags)
+docker_repo=hub.docker.com
 
 PALANTIR_FLAGS=(-Phadoop-cloud -Phadoop-palantir -Pkinesis-asl -Pkubernetes -Phive -Pyarn -Psparkr)
 
@@ -16,7 +17,7 @@ publish_artifacts() {
   ./build/mvn --settings $tmp_settings -DskipTests "${PALANTIR_FLAGS[@]}" deploy
 }
 
-make_dist() {
+make_and_publish_dist() {
   dist_name="$1"
   build_flags="$2"
   shift 2
@@ -26,6 +27,26 @@ make_dist() {
   curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T $file_name "https://api.bintray.com/content/palantir/releases/spark/${version}/org/apache/spark/spark-dist/${dist_version}/${file_name}"
 }
 
+make_and_publish_docker_images() {
+  dist_name="$1"
+
+  dist_version="${version}-${dist_name}"
+  file_name="spark-dist-${dist_version}.tgz"
+  dist_dir="dist-$dist_version/"
+  tar -zxvf "$file_name"
+  pushd "$dist_dir"
+
+  docker build -t "$docker_repo/spark-driver:$version" -f dockerfiles/driver/Dockerfile .
+  docker build -t "$docker_repo/spark-executor:$version" -f dockerfiles/executor/Dockerfile .
+
+  docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD" "$docker_repo"
+  docker push "$docker_repo/spark-driver:$version"
+  docker push "$docker_repo/spark-executor:$version"
+
+  popd
+}
+
 publish_artifacts
-make_dist hadoop-2.8.0-palantir3 "${PALANTIR_FLAGS[*]}" --clean
-make_dist without-hadoop "-Phadoop-provided -Pkubernetes -Phive -Pyarn -Psparkr" --clean
+make_and_publish_dist hadoop-2.8.0-palantir3 "${PALANTIR_FLAGS[*]}" --clean
+make_and_publish_dist without-hadoop "-Phadoop-provided -Pkubernetes -Phive -Pyarn -Psparkr" --clean
+make_and_publish_docker_images hadoop-2.8.0-palantir3
