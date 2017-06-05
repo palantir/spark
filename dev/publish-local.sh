e4698ead4a5fb755f766d02ece64dda0184d7afd
onursatici <onursatici@gmail.com>
2017-06-05 10:56:39 -0700

    get hadoop version from the pom file

--- dev/publish-local.sh
+++ dev/publish-local.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-version=$(git describe --tags)
+VERSION=$(git describe --tags)
+# get the hadoop version from the hadoop-palantir profile from pom.xml, and trim whitespaces
+HADOOP_VERSION=$(sed -ne '/<id>hadoop-palantir<\/id>/,/<\/profile>/s/<hadoop.version>\(.*\)<\/hadoop.version>/\1/p' ./pom.xml | tr -d '[:space:]')
 
 PALANTIR_FLAGS=(-Phadoop-cloud -Phadoop-palantir -Pkinesis-asl -Pkubernetes -Phive -Pyarn -Psparkr)
 
 MVN_LOCAL="~/.m2/repository"
 
 publish_artifacts() {
-  ./build/mvn versions:set -DnewVersion=$version
-  ./build/mvn -DskipTests "${PALANTIR_FLAGS[@]}" install
+  ./build/mvn versions:set -DnewVersion=$VERSION
+  ./build/mvn -DskipTests "${PALANTIR_FLAGS[@]}" install clean
 }
 
 make_dist() {
   dist_name="$1"
   build_flags="$2"
   shift 2
-  dist_version="${version}-${dist_name}"
+  dist_version="${VERSION}-${dist_name}"
   file_name="spark-dist-${dist_version}.tgz"
   ./dev/make-distribution.sh --name $dist_name --tgz "$@" $build_flags
-  mkdir $MVN_LOCAL/org/apache/spark/spark-dist/${dist_version} && \
-  cp -r $file_name $MVN_LOCAL/org/apache/spark/spark-dist/${dist_version}/${file_name}
+  mkdir -p $MVN_LOCAL/org/apache/spark/spark-dist/${dist_version} && \
+  cp $file_name $MVN_LOCAL/org/apache/spark/spark-dist/${dist_version}/
 }
 
 publish_artifacts
-make_dist hadoop-2.8.0-palantir3 "${PALANTIR_FLAGS[*]}" --clean
+make_dist $HADOOP_VERSION "${PALANTIR_FLAGS[*]}"
