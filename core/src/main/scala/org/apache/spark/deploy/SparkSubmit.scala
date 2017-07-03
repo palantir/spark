bbf992c8325b3f47ef946829011b684c13bb3a7e
Ilan Filonenko <ifilondz@gmail.com>
2017-07-03 12:25:59 -0700

    Python Bindings for launching PySpark Jobs from the JVM
    * Adding PySpark Submit functionality. Launching Python from JVM
    
    * Addressing scala idioms related to PR351
    
    * Removing extends Logging which was necessary for LogInfo
    
    * Refactored code to leverage the ContainerLocalizedFileResolver
    
    * Modified Unit tests so that they would pass
    
    * Modified Unit Test input to pass Unit Tests
    
    * Setup working environent for integration tests for PySpark
    
    * Comment out Python thread logic until Jenkins has python in Python
    
    * Modifying PythonExec to pass on Jenkins
    
    * Modifying python exec
    
    * Added unit tests to ClientV2 and refactored to include pyspark submission resources
    
    * Modified unit test check
    
    * Scalastyle
    
    * PR 348 file conflicts
    
    * Refactored unit tests and styles
    
    * further scala stylzing and logic
    
    * Modified unit tests to be more specific towards Class in question
    
    * Removed space delimiting for methods
    
    * Submission client redesign to use a step-based builder pattern.
    
    This change overhauls the underlying architecture of the submission
    client, but it is intended to entirely preserve existing behavior of
    Spark applications. Therefore users will find this to be an invisible
    change.
    
    The philosophy behind this design is to reconsider the breakdown of the
    submission process. It operates off the abstraction of "submission
    steps", which are transformation functions that take the previous state
    of the driver and return the new state of the driver. The driver's state
    includes its Spark configurations and the Kubernetes resources that will
    be used to deploy it.
    
    Such a refactor moves away from a features-first API design, which
    considers different containers to serve a set of features. The previous
    design, for example, had a container files resolver API object that
    returned different resolutions of the dependencies added by the user.
    However, it was up to the main Client to know how to intelligently
    invoke all of those APIs. Therefore the API surface area of the file
    resolver became untenably large and it was not intuitive of how it was
    to be used or extended.
    
    This design changes the encapsulation layout; every module is now
    responsible for changing the driver specification directly. An
    orchestrator builds the correct chain of steps and hands it to the
    client, which then calls it verbatim. The main client then makes any
    final modifications that put the different pieces of the driver
    together, particularly to attach the driver container itself to the pod
    and to apply the Spark configuration as command-line arguments.
    
    * Don't add the init-container step if all URIs are local.
    
    * Python arguments patch + tests + docs
    
    * Revert "Python arguments patch + tests + docs"
    
    This reverts commit 4533df2a03e2a8922988b0bd01691ad1f26e5d03.
    
    * Revert "Don't add the init-container step if all URIs are local."
    
    This reverts commit e103225d9ff54ca17692279cc6a7999f9b8c3265.
    
    * Revert "Submission client redesign to use a step-based builder pattern."
    
    This reverts commit 5499f6ddf9b42c0526f1dc053317afb38dc71294.
    
    * style changes
    
    * space for styling
    
    (cherry picked from commit befcf0a30651d0335bb57c242a824e43748db33f)
    
     Conflicts:
    	README.md
    	core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala

--- core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
@@ -279,8 +279,8 @@ object SparkSubmit extends CommandLineUtils {
     (clusterManager, deployMode) match {
       case (KUBERNETES, CLIENT) =>
         printErrorAndExit("Client mode is currently not supported for Kubernetes.")
-      case (KUBERNETES, CLUSTER) if args.isPython || args.isR =>
-        printErrorAndExit("Kubernetes does not currently support python or R applications.")
+      case (KUBERNETES, CLUSTER) if args.isR =>
+        printErrorAndExit("Kubernetes does not currently support R applications.")
       case (STANDALONE, CLUSTER) if args.isPython =>
         printErrorAndExit("Cluster deploy mode is currently not supported for python " +
           "applications on standalone clusters.")
@@ -372,7 +372,6 @@ object SparkSubmit extends CommandLineUtils {
         downloadFileList(_, targetDir, args.sparkProperties, hadoopConf)
       }.orNull
     }
-
 
     // If we're running a python app, set the main class to our specific python runner
     if (args.isPython && deployMode == CLIENT) {
@@ -651,8 +650,14 @@ object SparkSubmit extends CommandLineUtils {
 
     if (isKubernetesCluster) {
       childMainClass = "org.apache.spark.deploy.kubernetes.submit.Client"
-      childArgs += args.primaryResource
-      childArgs += args.mainClass
+      if (args.isPython) {
+        childArgs += args.primaryResource
+        childArgs += "org.apache.spark.deploy.PythonRunner"
+        childArgs += args.pyFiles
+      } else {
+        childArgs += args.primaryResource
+        childArgs += args.mainClass
+      }
       childArgs ++= args.childArgs
     }
 
