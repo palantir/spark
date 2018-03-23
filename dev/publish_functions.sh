5cd63fccff85016b13a647de3533f1de56be7dc9
Dan Sanduleac <dsanduleac@palantir.com>
2018-03-23 00:28:22 +0000

    Fix versions to use --first-parent

--- dev/publish_functions.sh
+++ dev/publish_functions.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 
 PALANTIR_FLAGS=(-Phadoop-cloud -Phadoop-palantir -Pkinesis-asl -Pkubernetes -Phive -Pyarn -Psparkr)
 
+get_version() {
+  git describe --tags --first-parent
+}
+
 set_version_and_package() {
+  version=$(get_version)
   ./build/mvn versions:set -DnewVersion="$version"
   ./build/mvn -DskipTests "${PALANTIR_FLAGS[@]}" package
 }
@@ -22,9 +27,10 @@ publish_artifacts() {
 make_dist() {
   build_flags="$1"
   shift 1
+  version=$(get_version)
   hadoop_name="hadoop-palantir"
   artifact_name="spark-dist_2.11-${hadoop_name}"
   file_name="spark-dist-${version}-${hadoop_name}.tgz"
   ./dev/make-distribution.sh --name "hadoop-palantir" --tgz "$@" $build_flags
-  curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T $file_name "https://api.bintray.com/content/palantir/releases/spark/${version}/org/apache/spark/${artifact_name}/${version}/${artifact_name}-${version}.tgz"
+  curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T "$file_name" "https://api.bintray.com/content/palantir/releases/spark/${version}/org/apache/spark/${artifact_name}/${version}/${artifact_name}-${version}.tgz"
 }
