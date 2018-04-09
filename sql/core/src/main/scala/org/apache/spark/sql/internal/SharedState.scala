94046d7f19c508d33309aeb6deba4d6e975697b9
Robert Kruszewski <robertk@palantir.com>
2018-04-08 21:19:34 -0700

    listing one

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
@@ -165,7 +165,7 @@ object SharedState extends Logging {
   private def externalCatalogClassName(conf: SparkConf): String = {
     conf.get(CATALOG_IMPLEMENTATION) match {
       case "hive" => HIVE_EXTERNAL_CATALOG_CLASS_NAME
-      case "in-memory" => classOf[InMemoryCatalog].getCanonicalName
+      case "in-memory" => classOf[ListingInMemoryCatalog].getCanonicalName
       case name => name
     }
   }
