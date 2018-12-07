329ea3109537d68b84fcd60a329e6782968e4303
Jeremy Liu <jeremyliu@palantir.com>
2018-12-07 13:06:18 -0800

    Restructure

--- core/src/main/scala/org/apache/spark/api/r/RRunner.scala
+++ core/src/main/scala/org/apache/spark/api/r/RRunner.scala
@@ -363,11 +363,11 @@ private[r] object RRunner {
     val rLibDir = condaEnv.map(conda =>
       RUtils.sparkRPackagePath(isDriver = false) :+ (conda.condaEnvDir + "/lib/R/library"))
       .getOrElse(RUtils.sparkRPackagePath(isDriver = false))
-      .filter(dir => new File(dir + "/SparkR/worker/" + script).exists)
+      .filter(dir => new File(dir).exists)
     if (rLibDir.isEmpty) {
       throw new SparkException("SparkR package is not installed on executor.")
     }
-    val rExecScript = rLibDir.head + "/SparkR/worker/" + script
+    val rExecScript = RUtils.getSparkRScript(rLibDir, "/SparkR/worker/" + script)
     val pb = new ProcessBuilder(Arrays.asList(rCommand, rOptions, rExecScript))
     // Activate the conda environment by setting the right env variables if applicable.
     condaEnv.map(_.activatedEnvironment()).map(_.asJava).foreach(pb.environment().putAll)
