464670ca8d5a479b1f3e6c13e4f4c65ec2ad730d
mcheah <mcheah@palantir.com>
2019-03-27 10:37:47 -0700

    Don't switch on initializing executor components based on local mode.

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -121,9 +121,10 @@ private[spark] class Executor(
     env.blockManager.initialize(conf.getAppId)
     env.metricsSystem.registerSource(executorSource)
     env.metricsSystem.registerSource(env.blockManager.shuffleMetricsSource)
-    // Initialize the ShuffleDataIO
-    env.shuffleDataIO.executor().intitializeExecutor(conf.getAppId, executorId)
   }
+
+  // Initialize the ShuffleDataIO
+  env.shuffleDataIO.executor().intitializeExecutor(conf.getAppId, executorId)
 
   // Whether to load classes in user jars before those in Spark jars
   private val userClassPathFirst = conf.get(EXECUTOR_USER_CLASS_PATH_FIRST)
