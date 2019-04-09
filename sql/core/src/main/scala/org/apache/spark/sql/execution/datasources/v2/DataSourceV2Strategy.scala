5346dcf9b695588bfddfd34f5ff0708f88c8e2d1
francis0407 <hanmingcong123@hotmail.com>
2019-04-09 21:45:46 +0800

    [SPARK-27411][SQL] DataSourceV2Strategy should not eliminate subquery
    In DataSourceV2Strategy, it seems we eliminate the subqueries by mistake after normalizing filters.
    We have a sql with a scalar subquery:
    
    ``` scala
    val plan = spark.sql("select * from t2 where t2a > (select max(t1a) from t1)")
    plan.explain(true)
    ```
    
    And we get the log info of DataSourceV2Strategy:
    ```
    Pushing operators to csv:examples/src/main/resources/t2.txt
    Pushed Filters:
    Post-Scan Filters: isnotnull(t2a#30)
    Output: t2a#30, t2b#31
    ```
    
    The `Post-Scan Filters` should contain the scalar subquery, but we eliminate it by mistake.
    ```
    == Parsed Logical Plan ==
    'Project [*]
    +- 'Filter ('t2a > scalar-subquery#56 [])
       :  +- 'Project [unresolvedalias('max('t1a), None)]
       :     +- 'UnresolvedRelation `t1`
       +- 'UnresolvedRelation `t2`
    
    == Analyzed Logical Plan ==
    t2a: string, t2b: string
    Project [t2a#30, t2b#31]
    +- Filter (t2a#30 > scalar-subquery#56 [])
       :  +- Aggregate [max(t1a#13) AS max(t1a)#63]
       :     +- SubqueryAlias `t1`
       :        +- RelationV2[t1a#13, t1b#14] csv:examples/src/main/resources/t1.txt
       +- SubqueryAlias `t2`
          +- RelationV2[t2a#30, t2b#31] csv:examples/src/main/resources/t2.txt
    
    == Optimized Logical Plan ==
    Filter (isnotnull(t2a#30) && (t2a#30 > scalar-subquery#56 []))
    :  +- Aggregate [max(t1a#13) AS max(t1a)#63]
    :     +- Project [t1a#13]
    :        +- RelationV2[t1a#13, t1b#14] csv:examples/src/main/resources/t1.txt
    +- RelationV2[t2a#30, t2b#31] csv:examples/src/main/resources/t2.txt
    
    == Physical Plan ==
    *(1) Project [t2a#30, t2b#31]
    +- *(1) Filter isnotnull(t2a#30)
       +- *(1) BatchScan[t2a#30, t2b#31] class org.apache.spark.sql.execution.datasources.v2.csv.CSVScan
    ```
    
    ut
    
    Closes #24321 from francis0407/SPARK-27411.
    
    Authored-by: francis0407 <hanmingcong123@hotmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Strategy.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Strategy.scala
@@ -108,12 +108,23 @@ object DataSourceV2Strategy extends Strategy with PredicateHelper {
     case PhysicalOperation(project, filters, relation: DataSourceV2Relation) =>
       val scanBuilder = relation.newScanBuilder()
 
+<<<<<<< HEAD
       val normalizedFilters = DataSourceStrategy.normalizeFilters(filters, relation.output)
+||||||| parent of 601fac2cb3... [SPARK-27411][SQL] DataSourceV2Strategy should not eliminate subquery
+      val normalizedFilters = DataSourceStrategy.normalizeFilters(
+        filters.filterNot(SubqueryExpression.hasSubquery), relation.output)
+=======
+      val (withSubquery, withoutSubquery) = filters.partition(SubqueryExpression.hasSubquery)
+      val normalizedFilters = DataSourceStrategy.normalizeFilters(
+        withoutSubquery, relation.output)
+>>>>>>> 601fac2cb3... [SPARK-27411][SQL] DataSourceV2Strategy should not eliminate subquery
 
       // `pushedFilters` will be pushed down and evaluated in the underlying data sources.
       // `postScanFilters` need to be evaluated after the scan.
       // `postScanFilters` and `pushedFilters` can overlap, e.g. the parquet row group filter.
-      val (pushedFilters, postScanFilters) = pushFilters(scanBuilder, normalizedFilters)
+      val (pushedFilters, postScanFiltersWithoutSubquery) =
+        pushFilters(scanBuilder, normalizedFilters)
+      val postScanFilters = postScanFiltersWithoutSubquery ++ withSubquery
       val (scan, output) = pruneColumns(scanBuilder, relation, project ++ postScanFilters)
       logInfo(
         s"""
