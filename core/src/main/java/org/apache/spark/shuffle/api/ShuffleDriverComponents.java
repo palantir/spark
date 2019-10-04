4dbb80ab33fcb090f152f943faa0893c156a3ccc
mcheah <mcheah@palantir.com>
2019-10-04 16:05:02 -0700

    Fix build

--- core/src/main/java/org/apache/spark/shuffle/api/ShuffleDriverComponents.java
+++ core/src/main/java/org/apache/spark/shuffle/api/ShuffleDriverComponents.java
@@ -19,8 +19,6 @@ package org.apache.spark.shuffle.api;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Optional;
-import org.apache.spark.storage.BlockManagerId;
 
 public interface ShuffleDriverComponents {
 
