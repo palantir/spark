19a1ab1099f9d6b9b85903bf227002d6a2ff6a24
Lorenzo Martini <lmartini@palantir.com>
2021-03-30 20:51:27 +0100

    Build dist before push

--- dev/publish.sh
+++ dev/publish.sh
@@ -6,5 +6,5 @@ FWDIR="$(cd "`dirname "${BASH_SOURCE[0]}"`"; pwd)"
 
 source "$FWDIR/publish_functions.sh"
 
-publish_artifacts | tee -a "/tmp/publish_artifacts.log"
 DONT_BUILD=true make_dist_and_deploy | tee -a "/tmp/make-dist.log"
+publish_artifacts | tee -a "/tmp/publish_artifacts.log"
