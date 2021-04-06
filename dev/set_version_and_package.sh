cab98fed74514c54c9192075234b344f2863e083
Lorenzo Martini <lmartini@palantir.com>
2021-04-06 19:05:17 +0100

    Add versioned

--- dev/set_version_and_package.sh
+++ dev/set_version_and_package.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail
 
+get_version() {
+  git describe --tags --first-parent
+}
+
+set_version_and_package() {
+  version=$(get_version)
+  ./build/mvn versions:set -DnewVersion="$version"
+  ./build/mvn -DskipTests "${PALANTIR_FLAGS[@]}" package
+}
+
 FWDIR="$(cd "`dirname "${BASH_SOURCE[0]}"`"; pwd)"
 
 source "$FWDIR/publish_functions.sh"
